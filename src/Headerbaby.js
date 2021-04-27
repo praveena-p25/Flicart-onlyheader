9a49182f35454f843d8f4877b44b0e25fc199447
praveena-p25 <80034428+praveena-p25@users.noreply.github.com>
2021-04-27 21:02:42 +0530

    Add files via upload

---
+++ src/Headerbaby.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const Headerbaby = () => {
+    return (
+        <div  class="main"> 
+    
+        <div className="headerlogos" >
+            <div className="medialogos">
+                
+            </div>    
+             
+            
+        </div>
+        </div>
+    )
+}
+
+export default Headerbaby;
